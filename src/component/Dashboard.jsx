28273db900ffc3b26e69b8f56e21b50664ad1601
Tino Phan <tinophan247@gmail.com>
2023-10-05 00:22:41 +0700

    u

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -21,7 +21,7 @@ function Dashboard() {
         <div className="p-8 flex items-center justify-center w-screen ">
           <div className="flex mb-4 gap-2">
             <img
-              className="w-12 h-12 rounded-full"
+              className="w-12 h-12 -ml-10 xl:ml-0 rounded-full"
               src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/350253193_640042791314518_4514004573895829249_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=fe8171&_nc_ohc=M9IVvG0T6SQAX-OjRza&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AfDkdst5wkSkz68w__pxdGSLqwxdlSGbEVy3PstPQcQyaQ&oe=65230E0C"
             />
             <p
--- src/redux/slices/postSlice.js
+++ src/redux/slices/postSlice.js
@@ -8,7 +8,7 @@ const postSlice = createSlice({
                 "avatar": "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/350253193_640042791314518_4514004573895829249_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=fe8171&_nc_ohc=M9IVvG0T6SQAX-OjRza&_nc_ht=scontent.fsgn2-6.fna&cb_e2o_trans=q&oh=00_AfDkdst5wkSkz68w__pxdGSLqwxdlSGbEVy3PstPQcQyaQ&oe=65230E0C",
                 "name": "Hiep Dinh",
                 "dateTime": "22 August",
-                "text": "Màu lá mạ và màu cỏ úa tôi gọi là chú./nRiêng màu vàng tôi gọi là chó",
+                "text": "Màu lá mạ và màu cỏ úa tôi gọi là chú.<br> Riêng màu vàng tôi gọi là chó",
                 "like": 27,
                 "comment": 14
             }
